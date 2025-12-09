e05f4b9b88c059551bfb8e4f797bae8dfe7caaf9
ashimaganja <ashimaganja98@gmail.com>
2025-12-08 22:28:08 -0500

    version switched to 3.4.12

---
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -0,0 +1,18 @@
+package com.example.cashcard;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcards")
+public class CashCardController {
+
+    @GetMapping("/{requestedId}")
+    public ResponseEntity<CashCard> findById(){
+        CashCard cashCard = new CashCard(1000L, 0.0);
+        return ResponseEntity.ok(cashCard);
+    }
+}
